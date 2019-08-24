69b1e55c55ab7fe028c1ebe30a1f227c5d2eacfc
Ganesh B <ganeshsurfs@gmail.com>
2019-08-24 07:43:07 +0530

    Adding minimal comments for usage in each file

--- 1 - Basics/10-functions-args-kwargs.py
+++ 1 - Basics/10-functions-args-kwargs.py
@@ -11,24 +11,49 @@
 # Arguments can be of any type, can also be a function
 
 def hello(name):
-    print('Hello ' + name)
+        print('Hello ' + name)
 
 hello('Alice')
 hello('Wonderland')
 
+# Arguments can also be caught as a sequence of arguments
 def printUnnamedArgs(*args):
-    for x in enumerate(args):
-        print(x)
+        print(args) # Note the missing *
+        # printUnnamedArgs(*args) 
+                # # Note the * above when passing as argument sequence again to function
+        for x in enumerate(args):
+                print(x)
 
 printUnnamedArgs([1, 2, 3], [4, 5, 6])
 
+# Keyword Arguments can also be caught as a sequence of keyword arguments
 def printKeyWordArgs(**kwargs):
-    for k, v in kwargs.items():
-        print(k, v)
+        print(kwargs) # Note the missing **
+        # printKeyWordArgs(**kwargs) 
+        # # Note the ** above when passing as argument sequence again to function
+        for k, v in kwargs.items():
+                print(k, v)
 
 printKeyWordArgs(a=10, b=20)
 
+# Can pass both *args and ** kwargs together
+# *args always take precedence in function definition
+# Try inverting it or mixing up arguments and keyword arguments in the function
+#       to see the behaviour
 def fncArgsKwargs(*args, **kwargs):
-     print('ARGS: {} and KWARGS: {}'.format(args, kwargs))
+        print('ARGS: {} and KWARGS: {}'.format(args, kwargs))
 
 fncArgsKwargs(1,2,3, a=1, b=2, c=3)
+# fncArgsKwargs(c=3, 1,2,3, a=1, b=2)
+# fncArgsKwargs(c=3, 1,2,b=2, 3, a=1)
+
+# Try running the inverse
+# def fncArgsKwargsInverse(**kwargs,*args):
+#         print('ARGS: {} and KWARGS: {}'.format(args, kwargs))
+# fncArgsKwargsInverse(a=1, b=2, c=3, 1,2,3)
+
+# fncArgsKwargsInverse will result in following error
+#   File "10-functions-args-kwargs.py", line 51
+#     def fncArgsKwargsInverse(**kwargs,*args):
+#                                       ^
+# SyntaxError: invalid syntax
