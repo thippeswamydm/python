b492fcf60ef3daa62d8e2f1cab0bdaf0ff621ad7
Ganesh Bhat <ganeshsurfs@gmail.com>
2019-08-27 19:49:29 +0530

    Adding comments to classes demos

--- 4 - Classes-inheritance-oops/16-classes-metaclasses-call-usage.py
+++ 4 - Classes-inheritance-oops/16-classes-metaclasses-call-usage.py
@@ -4,28 +4,39 @@
 # Concept taken from following
 # https://github.com/django/django/blob/master/django/db/models/base.py
 
+# Creating a Base meta class using type
 class ModelBase(type):
+    # Creating a method to be applied to all classed extending metaclass ModelBase
     def hello(cls):
         print("Test", type(cls))
 
+    # Overriding implementation of __init__ of type and returning a class
     def __init__(cls, name, bases, dct):
+        # Printing arguments
         print('bases', bases)
         print('name', name)
         print('dict', dct)
         print('cls.__dict__', cls.__dict__)
+        # Returning the class as is - No changes
         return super(ModelBase, cls).__init__(cls)
 
     def __call__(self, *args, **kwargs):
+        # Adding hello and sayHello method
         setattr(self, "hello", self.hello)
         setattr(self, "sayHello", self.hello)
+        # Returning the modified class with two new methods
         return super(ModelBase, self).__call__()
 
+
 class MyTest(metaclass=ModelBase):
-        def testhello(self):
-            self.sayHello()
+    # Creating a method testhello
+    def testhello(self):
+        self.sayHello()
 
 
+# Instantiating MyTest class extended using the metaclass ModelBase
 obj = MyTest()
+# Checking availability of attributes/methods
 obj.sayHello()
+obj.hello()
 obj.testhello()
-
--- 4 - Classes-inheritance-oops/17-classes-metaclasses-convert-attr-to-upper.py
+++ 4 - Classes-inheritance-oops/17-classes-metaclasses-convert-attr-to-upper.py
@@ -2,24 +2,31 @@
 
 # Concept taken from following
 # https://github.com/django/django/blob/master/django/db/models/base.py
+# Some code taken from a blog and a stackoverflow question. Untracked
 
 # Creating a metaclass extending type
 class UpperAttrMetaclass(type):
-
+    # Defining a public and a private attribute to be added to all classes
     someValue = "My public someValue attribute from Custom meta UpperAttrMetaclass"
     __privateValue = "My private attribute from Custom meta UpperAttrMetaclass"
 
+    # Defining custom method to be added to all classes
     def common_attribute_def(cls):
         print("Printing from a UpperAttrMetaclass.common_attribute_def to be added", cls.__hash__(cls))
 
+    # Defining custom method to be added to all classes
     def second_attribute_def(cls):
         print("Printing from a UpperAttrMetaclass.second_attribute_def to be added", cls.__hash__(cls))
 
+    # Defining __init__ method behaviour
+    # Adding a method in the implementation to the class object
     def __init__(cls, clsname, bases, dct):
         print("Initialising my init from __init__", cls.__hash__(cls))
         setattr(cls, 'common_attribute_def', cls.common_attribute_def)
         return super(UpperAttrMetaclass, cls).__init__(cls)
 
+    # Defining __call__ method behaviour
+    # Adding a method and two attributes in the implementation to the class object
     def __call__(cls, *args, **kwargs):
         print('args, kwargs from __call__', args, kwargs)
         setattr(cls, "second_attribute_def", cls.second_attribute_def)
@@ -27,9 +34,14 @@ class UpperAttrMetaclass(type):
         setattr(cls, "__privateValue", cls.__privateValue)
         return super(UpperAttrMetaclass, cls).__call__()
 
+    # Defining a __new__ implementation behaviour for the metaclass
+    # Converting all the attributes defined in the class to uppercase
+    # Converts only attributes and methods to uppercase defined inside the class extending the metaclass
+    # Try adding the common_attribute_def and see the behaviour - does not get added
     def __new__(cls, clsname, bases, dct):
         uppercase_attr = {}
         print('Printing bases from type base class: __new__', bases)
+        # Convert attributes to uppercase (basically the dict keys)
         for name, val in dct.items():
             if not name.startswith('__'):
                 uppercase_attr[name.upper()] = val
@@ -39,18 +51,23 @@ class UpperAttrMetaclass(type):
         # setattr(cls, 'common_attribute_def', cls.common_attribute_def)
         return super(UpperAttrMetaclass, cls).__new__(cls, clsname, bases, uppercase_attr)
 
+
 # Creating base class for a sample bases in meta class defs
 class BaseClassTwo:
+    # Defining a mybasetwo method
     def mybasetwo(self):
         print("Extended: In mybasetwo function from BaseClassTwo")
 
+
 # Creating base class for a sample bases in meta class defs
 class BaseClass:
+    # Defining a mybase method
     def mybase(self):
         print("Extended: In mybase function from BaseClass")
 
+
 # Creating MyNewsClass extending two base classes and using a meta class defined above
-class MyNewsClass(BaseClass, BaseClassTwo, metaclass = UpperAttrMetaclass):
+class MyNewsClass(BaseClass, BaseClassTwo, metaclass=UpperAttrMetaclass):
     # Defining the __init__ method
     def __int__(self):
         super()
@@ -59,6 +76,8 @@ class MyNewsClass(BaseClass, BaseClassTwo, metaclass = UpperAttrMetaclass):
     def MyNewClassDef(self):
         print("Usage: small caps attributes due to metaclass new_fn from MyNewsClass")
 
+
+# Instantiation of class using the metaclass UpperAttrMetaclass which extends two base classes
 obj = MyNewsClass()
 
 # Checks for availability of methods
@@ -67,5 +86,6 @@ obj.mybase()
 obj.mybasetwo()
 obj.common_attribute_def()
 obj.second_attribute_def()
+# Checks for availability of properties
 print(obj.someValue)
 print(obj.__privateValue)
