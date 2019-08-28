f602a82b4b3fd81db0b53ea8f5cadcdc8e98ffdc
Ganesh Bhat <ganeshsurfs@gmail.com>
2019-08-28 21:46:53 +0530

    Adding some extra code bases and comments to modules

--- 5 - Modules/5 - importing - folders/mainfile.py
+++ 5 - Modules/5 - importing - folders/mainfile.py
@@ -1,5 +1,7 @@
 # Describes usage of import statements and using a file function as a import
 
+# Import traps
+# http://python-notes.curiousefficiency.org/en/latest/python_concepts/import_traps.html
 # main module since no __name__ has been specified
 
 import sys
--- 5 - Modules/8 - importing - creating - allowing specific packages/mainfile.py
+++ 5 - Modules/8 - importing - creating - allowing specific packages/mainfile.py
@@ -1,4 +1,8 @@
-# main module
+# Describes usage of import statements and using a file function as a import
+
+# Import traps
+# http://python-notes.curiousefficiency.org/en/latest/python_concepts/import_traps.html
+# main module since no __name__ has been specified
 
 from foo import *
 
